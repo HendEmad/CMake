25ef84b9c9111486d5984f820d17836da89e0da0
HendEmad <32018228@hti.edu.eg>
2024-04-21 13:54:36 +0200

    Basic code

---
+++ firstProject/main.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+template <typename T>
+T add(T a, T b)
+{
+    return a + b;
+}
+
+int main(){
+    std::cout << "Hello from C++ with CMake on Windows" << std::endl;
+    std::cout << "The sum is: " << add(4, 6) << std::endl;
+    return 0;
+}
