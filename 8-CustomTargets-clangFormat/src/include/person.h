5253ec7a2128530a9c7251554371d81235ce583f
Hend Emad <32018228@hti.edu.eg>
2024-05-11 20:16:36 +0300

    commit 14

---
+++ 8-CustomTargets-clangFormat/src/include/person.h
@@ -0,0 +1,14 @@
+#include <string>
+
+class Person {
+ private:
+  std::string name;
+  int age;
+
+ public:
+  Person(const std::string& name, int age);
+  std::string getName() const;
+  void setName(const std::string& newName);
+  int getAge() const;
+  void setAge(int newAge);
+};
---
+++ 8-CustomTargets-clangFormat/src/person.cpp
@@ -0,0 +1,21 @@
+#include "C:\Users\Data-DCS\CMake\8-CustomTargets-clangFormat\src\include\person.h"
+
+Person::Person(const std::string& name, int age) : name(name), age(age) {}
+
+std::string Person::getName() const {
+  return name;
+}
+
+void Person::setName(const std::string& newName) {
+  name = newName;
+}
+
+void Person::setAge(int newAge)
+
+{
+  age = newAge;
+}
+
+int Person::getAge() const {
+  return age;
+}
