147f5b632b9fea4edd3c698c17228ac2953e9f78
Hend Emad <32018228@hti.edu.eg>
2024-05-24 23:23:54 +0300

    googletest

---
+++ 10-Testing_GoogleTest/src/calculator.cpp
@@ -0,0 +1,23 @@
+#include "calculator.h"
+#include <iostream>
+using namespace std;
+
+int calculator::add(int a, int b) {
+    return a + b;
+}
+
+int calculator::sub(int a, int b) {
+    return a - b;
+}
+
+int calculator::mul(int a, int b) {
+    return a * b;
+}
+
+int calculator::div(int a, int b) {
+    return a / b;
+}
+
+int calculator::mod(int a, int b) {
+    return a % b;
+}
---
+++ 10-Testing_GoogleTest/test/test_calc.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include "C:\Users\Data-DCS\CMake\10-Testing_GoogleTest\src\calculator.h"
+
+TEST(calculatrTest, Add) {
+    calculator calc;
+    EXPECT_EQ(calc.add(2, 3), 5);
+    EXPECT_EQ(calc.add(-2, 3), 1);
+    EXPECT_EQ(calc.add(0, 0), 0);
+}
+
+TEST(calculatrTest, Sub) {
+    calculator calc;
+    EXPECT_EQ(calc.sub(5, 3), 2);
+    EXPECT_EQ(calc.sub(3, 5), -2);
+    EXPECT_EQ(calc.sub(0, 0), 0);
+}
+
+TEST(calculatrTest, Mul) {
+    calculator calc;
+    EXPECT_EQ(calc.mul(2, 3), 6);
+    EXPECT_EQ(calc.mul(-2, 3), -6);
+    EXPECT_EQ(calc.mul(0, 0), 0);
+}
+
+TEST(calculatrTest, Div) {
+    calculator calc;
+    EXPECT_EQ(calc.div(6, 3), 2);
+    EXPECT_EQ(calc.div(5, 2), 2);
+}
+
+TEST(calculatrTest, Mod) {
+    calculator calc;
+    EXPECT_EQ(calc.mod(6, 4), 2);
+    EXPECT_EQ(calc.mod(5, 2), 1);
+}
