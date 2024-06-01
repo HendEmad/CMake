5d9310ceff86739952190f29e30fb1c641cd7de8
Hend Emad <32018228@hti.edu.eg>
2024-06-01 22:42:54 +0300

    conan_template

---
+++ 14-Project_templates/conan_template/src/lib1/lib1.h
@@ -0,0 +1,29 @@
+// lib1.hpp
+
+#ifndef LIB_H
+#define LIB1_H
+
+#pragma once
+
+namespace lib1 {
+
+class MyClass1 {
+public:
+  // Generate javadoc for this function
+  /**
+      @brief function1
+  */
+  void function1();
+  /**
+      @brief function2
+  */
+  void function2();
+  /**
+      @brief function3
+  */
+  void function3();
+};
+
+} // namespace lib1
+
+#endif // LIB1_H
---
+++ 14-Project_templates/conan_template/src/lib2/lib2.h
@@ -0,0 +1,27 @@
+// lib2.hpp
+#ifndef LIB2_H
+#define LIB2_H
+
+#pragma once
+
+namespace lib2 {
+
+class MyClass2 {
+public:
+  /**
+      @brief function1
+  */
+  void function1();
+  /**
+      @brief function2
+  */
+  void function2();
+  /**
+      @brief function3
+  */
+  void function3();
+};
+
+} // namespace lib2
+
+#endif // LIB2_H
